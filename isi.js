2728dd4de970b197afd8a7f0f2ac0edf4aa76160
zaskianurramadhani <zaskianurramadhani@gmail.com>
2024-10-02 14:18:34 +0700

    isi.js

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeIn("</center>");
+document.writeIn("hr width=600 color=Black>");
+document.writeIn("<h1> UNIVERSITAS LAMPUNG </h1>"); 
+document.writeIn("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
+document.writeIn("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h3>")
+document.writeIn("<hr width=600 color=Black>");
+document.writeIn("</center>");
